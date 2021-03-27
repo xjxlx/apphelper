c6c4fb185553ecb168d9eb2c415b89fdc5b55636
“xujixing@foxgoing.com” <19900713op>
2021-03-27 12:52:28 +0800

    测试滑动工具

---
+++ app/src/main/java/android/helper/widget/hm/ScrollHelperLayout.java
@@ -0,0 +1,27 @@
+package android.helper.widget.hm;
+
+import android.content.Context;
+import android.helper.base.BaseViewGroup;
+import android.util.AttributeSet;
+
+public class ScrollHelperLayout extends BaseViewGroup {
+
+    public ScrollHelperLayout(Context context) {
+        super(context);
+    }
+
+    public ScrollHelperLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public void initView(Context context, AttributeSet attrs) {
+        super.initView(context, attrs);
+
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+
+    }
+}
