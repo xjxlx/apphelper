01b40efae3dabd66c197d5c35e3d822e7b655868
“xujixing@foxgoing.com” <19900713op>
2021-04-06 16:31:12 +0800

    新增日历选择器

--- helper/src/main/java/android/helper/utils/DateUtil.java
+++ helper/src/main/java/android/helper/utils/DateUtil.java
@@ -1,10 +1,14 @@
 package android.helper.utils;
 
+import android.annotation.SuppressLint;
+import android.text.TextUtils;
+
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+@SuppressLint("SimpleDateFormat")
 public class DateUtil {
-    
+
     /**
      * @param pattern 指定格式，例如：yyyy-MM-dd HH:mm:ss
      * @return 获取当前的时间，返回String类型的字符串
@@ -15,5 +19,22 @@ public class DateUtil {
         String dateString = formatter.format(currentTime);
         return dateString;
     }
-    
+
+    /**
+     * @param pattern 指定格式，例如：yyyy-MM-dd HH:mm:ss
+     * @return 获取当前的时间，返回String类型的字符串
+     */
+    public static String getTimeForDate(Date date, String pattern) {
+        String result = "";
+        try {
+            if ((!TextUtils.isEmpty(pattern)) && (date != null)) {
+                SimpleDateFormat formatter = new SimpleDateFormat(pattern);
+                result = formatter.format(date);
+            }
+        } catch (Exception e) {
+            LogUtil.e("转换失败！");
+        }
+        return result;
+    }
+
 }
