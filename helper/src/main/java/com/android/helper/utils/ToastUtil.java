fc2b202237b7acb96361851f488cd0c0beb21fb3
xjxliuxing@163.com <xjxliuxing@163.com>
2021-04-28 23:38:03 +0800

    通道的等级

--- helper/src/main/java/com/android/helper/utils/ToastUtil.java
+++ helper/src/main/java/com/android/helper/utils/ToastUtil.java
@@ -1,7 +1,7 @@
 package com.android.helper.utils;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
+import android.app.Application;
 import android.text.TextUtils;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -14,19 +14,19 @@ import com.android.helper.app.BaseApplication;
 
 @SuppressLint("StaticFieldLeak")
 public class ToastUtil {
-    
+
     private static final String TAG = "ToastUtil";
     private static Toast toast;
     private static int yOffset;
-    private static Context context;
+    private static Application context;
     private static View view;
-    
+
     /**
      * 私有化构造
      */
     private ToastUtil() {
     }
-    
+
     /**
      * 强大的吐司，能够连续弹的吐司,默认弹出在屏幕底部5分之一的位置
      *
@@ -36,14 +36,14 @@ public class ToastUtil {
         if (context == null) {
             context = BaseApplication.getApplication();
         }
-        
+
         if (yOffset <= 0) {
             int screenHeight = ScreenUtil.getScreenHeight(context);
             yOffset = screenHeight / 5;
         }
         show(text, Toast.LENGTH_SHORT, Gravity.BOTTOM, 0, yOffset);
     }
-    
+
     /**
      * @param text     内容
      * @param duration 时间
@@ -53,24 +53,24 @@ public class ToastUtil {
      */
     @SuppressLint("InflateParams")
     public static void show(String text, int duration, int gravity, int xOffset, int yOffset) {
-        
+
         if (context == null) {
             context = BaseApplication.getApplication();
         }
-        
+
         if (context == null) {
             LogUtil.e(TAG, "context为空！");
             return;
         }
-        
+
         if (TextUtils.isEmpty(text)) {
             return;
         }
-        
+
         if (toast != null) {
             toast.cancel();
         }
-        
+
         toast = new Toast(context);
         if (view == null) {
             view = LayoutInflater.from(context).inflate(R.layout.widget_toast, null);
@@ -78,13 +78,13 @@ public class ToastUtil {
         TextView textView = view.findViewById(R.id.message);
         // 4:设置布局的内容
         textView.setText(text);
-        
+
         // 5:设置Toast的参数
         toast.setGravity(gravity, xOffset, yOffset);
         toast.setView(view);
-        
+
         toast.setDuration(duration);
         toast.show();
     }
-    
+
 }
