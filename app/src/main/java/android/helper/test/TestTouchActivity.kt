74bcb8c963b9be5acdfbbd08dc5dccedd4c4bf7f
xjxliuxing@163.com <xjxliuxing@163.com>
2021-05-31 09:06:18 +0800

    ----

--- app/src/main/java/android/helper/test/TestTouchActivity.kt
+++ app/src/main/java/android/helper/test/TestTouchActivity.kt
@@ -5,15 +5,12 @@ import android.os.Handler
 import android.os.Looper
 import android.os.Message
 import android.view.MotionEvent
-import android.view.View
 import com.android.helper.base.BaseTitleActivity
 import com.android.helper.utils.LogUtil
 import com.android.helper.utils.ResourceUtil
 import kotlinx.android.synthetic.main.activity_test_touch.*
 
 class TestTouchActivity : BaseTitleActivity() {
-
-    val Tag = javaClass.simpleName
 
     override fun getTitleLayout(): Int {
         return R.layout.activity_test_touch
@@ -26,20 +23,6 @@ class TestTouchActivity : BaseTitleActivity() {
 
     override fun initListener() {
         super.initListener()
-
-        rl_root.setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-
-                return false;
-            }
-        })
-
-        tv_text.setOnTouchListener(object : View.OnTouchListener {
-            override fun onTouch(v: View?, event: MotionEvent?): Boolean {
-
-                return false;
-            }
-        })
 
         val thread = object : Thread() {
             override fun run() {
@@ -59,23 +42,22 @@ class TestTouchActivity : BaseTitleActivity() {
             }
         }
         thread.start()
-
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent): Boolean {
         when (ev.action) {
-            MotionEvent.ACTION_DOWN -> LogUtil.e(Tag, "dispatchTouchEvent--->down")
-            MotionEvent.ACTION_MOVE -> LogUtil.e(Tag, "dispatchTouchEvent--->move")
-            MotionEvent.ACTION_UP -> LogUtil.e(Tag, "dispatchTouchEvent--->up")
+            MotionEvent.ACTION_DOWN -> LogUtil.e(TAG, "dispatchTouchEvent--->down")
+            MotionEvent.ACTION_MOVE -> LogUtil.e(TAG, "dispatchTouchEvent--->move")
+            MotionEvent.ACTION_UP -> LogUtil.e(TAG, "dispatchTouchEvent--->up")
         }
         return super.dispatchTouchEvent(ev)
     }
 
     override fun onTouchEvent(event: MotionEvent): Boolean {
         when (event.action) {
-            MotionEvent.ACTION_DOWN -> LogUtil.e(Tag, "onTouchEvent--->down")
-            MotionEvent.ACTION_MOVE -> LogUtil.e(Tag, "onTouchEvent--->move")
-            MotionEvent.ACTION_UP -> LogUtil.e(Tag, "onTouchEvent--->up")
+            MotionEvent.ACTION_DOWN -> LogUtil.e(TAG, "onTouchEvent--->down")
+            MotionEvent.ACTION_MOVE -> LogUtil.e(TAG, "onTouchEvent--->move")
+            MotionEvent.ACTION_UP -> LogUtil.e(TAG, "onTouchEvent--->up")
         }
         return super.onTouchEvent(event)
     }
--- app/src/main/java/android/helper/widget/hm/ViewGroupTouch.java
+++ app/src/main/java/android/helper/widget/hm/ViewGroupTouch.java
@@ -1,8 +1,13 @@
 package android.helper.widget.hm;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.NonNull;
@@ -12,6 +17,13 @@ import com.android.helper.utils.LogUtil;
 
 public class ViewGroupTouch extends RelativeLayout {
     public final String Tag = getClass().getSimpleName();
+    private Rect mRect;
+    private Paint mPaint;
+    private View mChildView;
+    private float mStartX;
+    private float mStartY;
+    private float mDx;
+    private float mDy;
 
     public ViewGroupTouch(@NonNull Context context) {
         super(context);
@@ -23,7 +35,25 @@ public class ViewGroupTouch extends RelativeLayout {
     }
 
     private void initView(Context context) {
+        mRect = new Rect(200, 400, 800, 1200);
+        mPaint = new Paint();
+        mPaint.setColor(Color.BLUE);
+        mPaint.setAntiAlias(true);
 
+        // 自动调用onDraw方法
+        setWillNotDraw(false);
+    }
+
+    @Override
+    protected void onFinishInflate() {
+        super.onFinishInflate();
+        int childCount = getChildCount();
+        if (childCount > 0) {
+            View childAt = getChildAt(0);
+            if (childAt != null) {
+                mChildView = childAt;
+            }
+        }
     }
 
     @Override
@@ -31,7 +61,8 @@ public class ViewGroupTouch extends RelativeLayout {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 LogUtil.e(Tag, "dispatchTouchEvent--->down");
-                break;
+                return true;
+//                break;
 
             case MotionEvent.ACTION_MOVE:
                 LogUtil.e(Tag, "dispatchTouchEvent--->move");
@@ -51,34 +82,98 @@ public class ViewGroupTouch extends RelativeLayout {
             case MotionEvent.ACTION_DOWN:
                 LogUtil.e(Tag, "onInterceptTouchEvent--->down");
                 return true;
+//                break;
 
             case MotionEvent.ACTION_MOVE:
                 LogUtil.e(Tag, "onInterceptTouchEvent--->move");
+
+//                if (mChildView != null) {
+//                    int left = mChildView.getLeft();
+//                    int top = mChildView.getTop();
+//                    int right = mChildView.getRight();
+//                    int bottom = mChildView.getBottom();
+//
+//                    boolean isMove = false;
+//
+//                    if (left + mDx < mRect.left) {
+//                        ToastUtil.show("停止移动");
+//                        isMove = false;
+//                    } else {
+//                        isMove = true;
+//                    }
+//
+//                    LogUtil.e(" isMove:" + isMove);
+//                    return isMove;
+//                }
+
                 break;
 
             case MotionEvent.ACTION_UP:
                 LogUtil.e(Tag, "onInterceptTouchEvent--->up");
-                break;
+                return false;
+//                break;
         }
 
         return super.onInterceptTouchEvent(ev);
+//        return false;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        LogUtil.e("left:" + mRect.left + " top:" + mRect.top + " right:" + mRect.right + " bottom:" + mRect.bottom);
+
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
                 LogUtil.e(Tag, "onTouchEvent--->down");
+
+                mStartX = event.getX();
+                mStartY = event.getY();
                 break;
 
             case MotionEvent.ACTION_MOVE:
                 LogUtil.e(Tag, "onTouchEvent--->move");
+
+                float x = event.getX();
+                float y = event.getY();
+
+                // 获取偏移值
+                mDx = x - mStartX;
+                mDy = y - mStartY;
+
+                // 把后面的值赋值给开始的值
+                mStartX = x;
+                mStartY = y;
+
+                LogUtil.e("------>dx:" + mDx + "   dy:" + mDy);
+                if (mChildView != null) {
+                    int left = mChildView.getLeft();
+                    int top = mChildView.getTop();
+                    int right = mChildView.getRight();
+                    int bottom = mChildView.getBottom();
+
+                    LogUtil.e("left:" + left + " top:" + top + " right:" + right + " bottom:" + bottom);
+
+                    mChildView.layout(
+                            (int) (left + mDx),
+                            (int) (top + mDy),
+                            (int) (right + mDx),
+                            (int) (bottom + mDy)
+                    );
+                }
                 break;
 
             case MotionEvent.ACTION_UP:
                 LogUtil.e(Tag, "onTouchEvent--->up");
                 break;
         }
-        return super.onTouchEvent(event);
+//        return super.onTouchEvent(event);
+        return true;
     }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawRect(mRect, mPaint);
+    }
+
 }
