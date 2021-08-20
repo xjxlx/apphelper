2828ecd4d8c565a8c21080ea9c5eebee380f9dcb
xjxliuxing@163.com <19900713op>
2021-08-20 19:57:19 +0800

    保活程序的大概封装以及使用

--- app/src/main/java/android/helper/test/app/AppLifecycleActivity.java
+++ app/src/main/java/android/helper/test/app/AppLifecycleActivity.java
@@ -36,7 +36,7 @@ import java.util.List;
  * 3：打开后台的允许运行功能
  * <p>
  * 4:在8.0以下，使用一个像素的activity去保活
- * 5:Jobservice 轮询拉活
+ * 5:JobService 轮询拉活
  * 6：账号拉活
  */
 public class AppLifecycleActivity extends BaseActivity {
