20058d96a0452b5c6f49a5518d82a10bb8e24e90
xjxliuxing@163.com <19900713op>
2021-08-24 15:02:12 +0800

    流程一切正常

--- src/main/java/com/android/helper/app/CommonConstant.java
+++ src/main/java/com/android/helper/app/CommonConstant.java
@@ -6,10 +6,4 @@ public class CommonConstant {
      * 状态栏高度
      */
     public static final String KEY_STATUS_BAR_HEIGHT = "key_status_bar_height";
-
-    /**
-     * 状态栏高度
-     */
-    public static final String KEY_STATUS_BAR_HEIGHT_3 = "key_status_bar_height";
-
 }
