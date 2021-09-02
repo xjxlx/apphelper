202a05357333763219a0c295f9a58da4e191a700
xjxliuxing@163.com <xjxliuxing@163.com>
2021-09-02 22:46:02 +0800

    提交“

--- src/main/java/com/android/helper/base/BaseRecycleAdapter.java
+++ src/main/java/com/android/helper/base/BaseRecycleAdapter.java
@@ -1,6 +1,6 @@
 package com.android.helper.base;
 
-import android.content.Context;
+import android.app.Activity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,10 +9,7 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.android.helper.interfaces.listener.OnItemClickListener;
-import com.android.helper.interfaces.listener.OnRecycleLoadCompletedListener;
 import com.android.helper.utils.LogUtil;
-
-import org.jetbrains.annotations.NotNull;
 
 import java.util.List;
 
@@ -24,17 +21,16 @@ import java.util.List;
  */
 public abstract class BaseRecycleAdapter<T, E extends BaseVH> extends RecyclerView.Adapter<E> {
 
-    protected Context mContext;
+    protected Activity mContext;
     protected List<T> mList;
 
     protected OnItemClickListener<T> mItemClickListener;
-    protected OnRecycleLoadCompletedListener<E> mCompletedListener;
 
-    public BaseRecycleAdapter(Context mContext) {
+    public BaseRecycleAdapter(Activity mContext) {
         this.mContext = mContext;
     }
 
-    public BaseRecycleAdapter(Context mContext, List<T> mList) {
+    public BaseRecycleAdapter(Activity mContext, List<T> mList) {
         this.mContext = mContext;
         this.mList = mList;
     }
@@ -66,24 +62,8 @@ public abstract class BaseRecycleAdapter<T, E extends BaseVH> extends RecyclerVi
         return createViewHolder(inflate);
     }
 
-    @Override
-    public void onBindViewHolder(@NonNull @NotNull E holder, int position) {
-        // 最后一个数据加载完成的通知
-        if ((mList != null) && (mCompletedListener != null)) {
-            if (position == (mList.size() - 1)) {
-                mCompletedListener.onLoadComplete(holder, position);
-            }
-        }
-    }
-
     public void setItemClickListener(OnItemClickListener<T> mOnItemClickListener) {
         this.mItemClickListener = mOnItemClickListener;
     }
 
-    /**
-     * @param completedListener 数据加载完成的回调
-     */
-    public void setOnLoadComplete(OnRecycleLoadCompletedListener<E> completedListener) {
-        this.mCompletedListener = completedListener;
-    }
 }
