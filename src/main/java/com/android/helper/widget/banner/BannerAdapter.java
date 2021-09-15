bed68c3f23160a7239e1d285db7df142b4fb01f9
xjxliuxing@163.com <19900713op>
2021-09-15 21:29:27 +0800

    优化banner

--- src/main/java/com/android/helper/widget/banner/BannerAdapter.java
+++ src/main/java/com/android/helper/widget/banner/BannerAdapter.java
@@ -1,16 +1,16 @@
 package com.android.helper.widget.banner;
 
+import android.annotation.SuppressLint;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
 
 import com.android.helper.R;
-import com.android.helper.common.CommonConstants;
-import com.android.helper.utils.LogUtil;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -32,10 +32,7 @@ public class BannerAdapter<T> extends PagerAdapter {
 
     @Override
     public int getCount() {
-        if (mListData.size() == 1) {
-            return mListData.size();
-        }
-        return CommonConstants.BANNER_LENGTH;
+        return mListData == null ? 0 : mListData.size();
     }
 
     @Override
@@ -45,29 +42,20 @@ public class BannerAdapter<T> extends PagerAdapter {
 
     //:初始化每个Item的实布局，类似于getview
     // :viewpager会默认加载三个布局，上一页，本业，和下一页，其他页面会自动销毁，防止内存溢出
+    @SuppressLint("SetTextI18n")
     @NotNull
     @Override
     public Object instantiateItem(@NotNull ViewGroup container, int position) {
-        LogUtil.e("instantiateItem:");
+        // LogUtil.e("instantiateItem:");
         View view = null;
         //:1:首先要拿到position的位置，为了避免角标越界，进行取余运算
         if (mListData != null) {
-            position = position % mListData.size();
             //:2:设置对象
             view = LayoutInflater.from(container.getContext()).inflate(R.layout.base_banner, null);
 
             ImageView imageView = view.findViewById(R.id.iv_banner_image);
-            ViewGroup.LayoutParams params = imageView.getLayoutParams();
-            if ((mBannerView != null) && (params != null)) {
-                int width = mBannerView.getWidth();
-                int height = mBannerView.getHeight();
-
-                // 设置view的宽高
-                params.width = width;
-                params.height = height;
-                imageView.setLayoutParams(params);
-                imageView.setAdjustViewBounds(true);
-            }
+            TextView tvPosition = view.findViewById(R.id.tv_position);
+            tvPosition.setText("" + position);
 
             // 此处为了兼容多种处理方式，以一个imageView的形式，把图片给传递出去，让用户手动选择怎么去处理
             T t = mListData.get(position);
@@ -75,10 +63,9 @@ public class BannerAdapter<T> extends PagerAdapter {
                 mLoadListener.onLoadView(imageView, t);
             }
 
-            int finalPosition = position;
             view.setOnClickListener(v -> {
                 if (mItemClickListener != null) {
-                    mItemClickListener.onItemClick(null, imageView, finalPosition, t);
+                    mItemClickListener.onItemClick(null, imageView, position, t);
                 }
             });
 
