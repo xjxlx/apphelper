09449c963adae5438de030e2430f9514fcd72f10
xujixing01@souche.com <19900713op>
2021-11-29 10:04:34 +0800

    兼容bingActivity

--- src/main/java/com/android/helper/base/BaseBindingActivity.java
+++ src/main/java/com/android/helper/base/BaseBindingActivity.java
@@ -1,10 +1,13 @@
 package com.android.helper.base;
 
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.viewbinding.ViewBinding;
 
 import com.android.helper.interfaces.BindingViewListener;
+import com.android.helper.utils.TextViewUtil;
 
 /**
  * 封装viewBinding 的activity 的基类
@@ -36,6 +39,33 @@ public abstract class BaseBindingActivity<T extends ViewBinding> extends BaseAct
     @Override
     protected int getBaseLayout() {
         return 0;
+    }
+
+    /**
+     * 设置标题
+     *
+     * @param tv_title 标题的id
+     * @param title    标题的内容
+     */
+    protected void setTitleContent(int tv_title, String title) {
+        if (tv_title != 0 && (!TextUtils.isEmpty(title))) {
+            View view = findViewById(tv_title);
+            if (view instanceof TextView) {
+                TextViewUtil.setText((TextView) view, title);
+            }
+        }
+    }
+
+    /**
+     * 设置返回按钮的点击事件
+     */
+    protected void setTitleBack(int tv_title_bar_left) {
+        if (tv_title_bar_left != 0) {
+            View view = findViewById(tv_title_bar_left);
+            if (view != null) {
+                view.setOnClickListener(v -> finish());
+            }
+        }
     }
 
 }
