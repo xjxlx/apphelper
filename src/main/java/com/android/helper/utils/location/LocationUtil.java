e597a0c8f9c519ca48e2bd93d812acd2a0a89125
xjxliuxing@163.com <xjxliuxing@163.com>
2021-12-03 00:23:39 +0800

    定位的修改“

--- src/main/java/com/android/helper/utils/location/LocationUtil.java
+++ src/main/java/com/android/helper/utils/location/LocationUtil.java
@@ -55,57 +55,60 @@ public class LocationUtil implements BaseLifecycleObserver {
      * @param reGeocodeResultListener 解析出来的事件回调
      */
     public static void getAddressForLatitude(Context context, double latitude, double longitude, String searchType, ReGeocodeResultListener reGeocodeResultListener) {
-        // 设置地理编码（正向和逆向）查询监听
-        GeocodeSearch geocodeSearch = new GeocodeSearch(context);
-        LatLonPoint latLonPoint = new LatLonPoint(latitude, longitude);
-        // 第一个参数表示一个LatLng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
-        RegeocodeQuery query = new RegeocodeQuery(latLonPoint, 200f, searchType);
-        // 设置监听
-        geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
-            @Override
-            public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int rCode) {
-                // 返回结果成功或者失败的响应码。1000为成功，其他为失败
-                if (rCode == 1000) {
-                    if (reGeocodeResultListener != null) {
-                        reGeocodeResultListener.onReGeocodeSearched(regeocodeResult);
+        try {
+            // 设置地理编码（正向和逆向）查询监听
+            GeocodeSearch geocodeSearch = new GeocodeSearch(context);
+            LatLonPoint latLonPoint = new LatLonPoint(latitude, longitude);
+            // 第一个参数表示一个LatLng，第二参数表示范围多少米，第三个参数表示是火系坐标系还是GPS原生坐标系
+            RegeocodeQuery query = new RegeocodeQuery(latLonPoint, 200f, searchType);
+            // 设置监听
+            geocodeSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+                @Override
+                public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int rCode) {
+                    // 返回结果成功或者失败的响应码。1000为成功，其他为失败
+                    if (rCode == 1000) {
+                        if (reGeocodeResultListener != null) {
+                            reGeocodeResultListener.onReGeocodeSearched(regeocodeResult);
+                        }
+                    } else {
+                        LogUtil.e("逆地理编码获取地址失败：code:" + rCode);
                     }
-                } else {
-                    LogUtil.e("逆地理编码获取地址失败：code:" + rCode);
+                    //  ReGeocodeAddress reGeocodeAddress = reGeocodeResult.getReGeocodeAddress();
+                    //  String formatAddress = reGeocodeAddress.getFormatAddress();
+                    //  LogUtil.e("查询经纬度对应详细地址:" + formatAddress);
+
+                    // ReGeocodeAddress reGeocodeAddress = reGeocodeResult.getReGeocodeAddress();
+                    // getAdCode() 返回逆地理编码结果所在区（县）的编码。
+                    // getAois() 返回AOI（面状数据）的数据，如POI名称、区域编码、中心点坐标、POI类型等。
+                    // getBuilding() 逆地理编码返回的建筑物名称。
+                    // getBusinessAreas() 返回商圈对象列表，若服务没有相应数据，则返回列表长度为0。
+                    // getCity() 逆地理编码返回的所在城市名称。
+                    // getCityCode() 返回逆地理编码结果所在城市编码。
+                    // getCountry() 获取国家名称。
+                    // getCountryCode() 海外生效 国家简码
+                    // getCrossroads() 逆地理编码返回的交叉路口列表。
+                    // getDistrict() 逆地理编码返回的所在区（县）名称。
+                    // getFormatAddress() 逆地理编码返回的格式化地址。
+                    // getNeighborhood() 逆地理编码返回的社区名称。
+                    // getPois() 逆地理编码返回的POI(兴趣点)列表。
+                    // getProvince() 逆地理编码返回的所在省名称、直辖市的名称 。
+                    // getRoads() 逆地理编码返回的道路列表。
+                    // getStreetNumber() 逆地理编码返回的门牌信息。
+                    // getTowncode() 返回乡镇街道编码。
+                    // getTownship() 逆地理编码返回的乡镇名称。
+
                 }
-                //  ReGeocodeAddress reGeocodeAddress = reGeocodeResult.getReGeocodeAddress();
-                //  String formatAddress = reGeocodeAddress.getFormatAddress();
-                //  LogUtil.e("查询经纬度对应详细地址:" + formatAddress);
 
-                // ReGeocodeAddress reGeocodeAddress = reGeocodeResult.getReGeocodeAddress();
-                // getAdCode() 返回逆地理编码结果所在区（县）的编码。
-                // getAois() 返回AOI（面状数据）的数据，如POI名称、区域编码、中心点坐标、POI类型等。
-                // getBuilding() 逆地理编码返回的建筑物名称。
-                // getBusinessAreas() 返回商圈对象列表，若服务没有相应数据，则返回列表长度为0。
-                // getCity() 逆地理编码返回的所在城市名称。
-                // getCityCode() 返回逆地理编码结果所在城市编码。
-                // getCountry() 获取国家名称。
-                // getCountryCode() 海外生效 国家简码
-                // getCrossroads() 逆地理编码返回的交叉路口列表。
-                // getDistrict() 逆地理编码返回的所在区（县）名称。
-                // getFormatAddress() 逆地理编码返回的格式化地址。
-                // getNeighborhood() 逆地理编码返回的社区名称。
-                // getPois() 逆地理编码返回的POI(兴趣点)列表。
-                // getProvince() 逆地理编码返回的所在省名称、直辖市的名称 。
-                // getRoads() 逆地理编码返回的道路列表。
-                // getStreetNumber() 逆地理编码返回的门牌信息。
-                // getTowncode() 返回乡镇街道编码。
-                // getTownship() 逆地理编码返回的乡镇名称。
+                @Override
+                public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
 
-            }
+                }
 
-            @Override
-            public void onGeocodeSearched(GeocodeResult geocodeResult, int i) {
-
-            }
-
-        });
-        //异步查询
-        geocodeSearch.getFromLocationAsyn(query);
+            });
+            //异步查询
+            geocodeSearch.getFromLocationAsyn(query);
+        } catch (Exception ignored) {
+        }
     }
 
     /**
@@ -115,45 +118,48 @@ public class LocationUtil implements BaseLifecycleObserver {
      * @param resultListener 返回数据的监听
      */
     public static void getLocationForAddress(Context context, String address, String city, GeocodeResultListener resultListener) {
-        GeocodeSearch geocoderSearch = new GeocodeSearch(context);
-        geocoderSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
-            @Override
-            public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
+        try {
+            GeocodeSearch geocoderSearch = new GeocodeSearch(context);
+            geocoderSearch.setOnGeocodeSearchListener(new GeocodeSearch.OnGeocodeSearchListener() {
+                @Override
+                public void onRegeocodeSearched(RegeocodeResult regeocodeResult, int i) {
 
-            }
-
-            @Override
-            public void onGeocodeSearched(GeocodeResult geocodeResult, int rCode) {
-                // 定位信息查询
-                if (rCode == 1000) {
-                    //   List<GeocodeAddress> geocodeAddressList = geocodeResult.getGeocodeAddressList(); 返回地理编码搜索的地理结果。
-                    //   getAdcode()  地理编码返回的区域编码。
-                    //   getBuilding() 地理编码返回的建筑物名称。
-                    //   getCity() 地理编码返回的所在城市名称。
-                    //   getCountry() 海外生效 国家名称
-                    //   getDistrict() 地理编码返回的所在区（县）名称。
-                    //   getFormatAddress() 地理编码返回的格式化地址。
-                    //   getLatLonPoint() 地理编码返回的经纬度坐标。
-                    //   getLevel() 地理编码返回的匹配级别。
-                    //   getNeighborhood() 地理编码返回的社区名称。
-                    //   getPostcode() 海外生效 邮政编码
-                    //   getProvince() 地理编码返回的所在省名称、直辖市的名称 。
-                    //   getTownship() 地理编码返回的乡镇名称。
-                    //   setCountry(java.lang.String country) 海外生效 国家名称
-                    //   setPostcode(java.lang.String postcode) 海外生效 邮政编码
-
-                    if (resultListener != null) {
-                        resultListener.onGeocodeSearched(geocodeResult);
-                    }
-                } else {
-                    LogUtil.e("地址转换经纬度，查询失败：code:" + rCode);
                 }
-            }
-        });
 
-        // name表示地址，第二个参数表示查询城市，city - 可选值：cityname（中文或中文全拼）、citycode、adcode。如传入null或空字符串则为“全国”，
-        GeocodeQuery query = new GeocodeQuery(address, city);
-        geocoderSearch.getFromLocationNameAsyn(query);
+                @Override
+                public void onGeocodeSearched(GeocodeResult geocodeResult, int rCode) {
+                    // 定位信息查询
+                    if (rCode == 1000) {
+                        //   List<GeocodeAddress> geocodeAddressList = geocodeResult.getGeocodeAddressList(); 返回地理编码搜索的地理结果。
+                        //   getAdcode()  地理编码返回的区域编码。
+                        //   getBuilding() 地理编码返回的建筑物名称。
+                        //   getCity() 地理编码返回的所在城市名称。
+                        //   getCountry() 海外生效 国家名称
+                        //   getDistrict() 地理编码返回的所在区（县）名称。
+                        //   getFormatAddress() 地理编码返回的格式化地址。
+                        //   getLatLonPoint() 地理编码返回的经纬度坐标。
+                        //   getLevel() 地理编码返回的匹配级别。
+                        //   getNeighborhood() 地理编码返回的社区名称。
+                        //   getPostcode() 海外生效 邮政编码
+                        //   getProvince() 地理编码返回的所在省名称、直辖市的名称 。
+                        //   getTownship() 地理编码返回的乡镇名称。
+                        //   setCountry(java.lang.String country) 海外生效 国家名称
+                        //   setPostcode(java.lang.String postcode) 海外生效 邮政编码
+
+                        if (resultListener != null) {
+                            resultListener.onGeocodeSearched(geocodeResult);
+                        }
+                    } else {
+                        LogUtil.e("地址转换经纬度，查询失败：code:" + rCode);
+                    }
+                }
+            });
+
+            // name表示地址，第二个参数表示查询城市，city - 可选值：cityname（中文或中文全拼）、citycode、adcode。如传入null或空字符串则为“全国”，
+            GeocodeQuery query = new GeocodeQuery(address, city);
+            geocoderSearch.getFromLocationNameAsyn(query);
+        } catch (Exception ignored) {
+        }
     }
 
     /**
@@ -264,71 +270,73 @@ public class LocationUtil implements BaseLifecycleObserver {
      * @param listener 数据返回的监听
      */
     private void getLocalInfo(Context context, LocationListener listener) {
-        if (mLocationClient == null) {
-            mLocationClient = new AMapLocationClient(context);
-        }
-
-        //初始化定位参数
-        if (mLocationOption == null) {
-            mLocationOption = new AMapLocationClientOption();
-        }
-        //设置返回地址信息，默认为true
-        mLocationOption.setNeedAddress(true);
-        //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
-        mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
-        if (isLoop) {
-            //设置定位间隔,单位毫秒,默认为2000ms
-            if (interval == 0) {
-                mLocationOption.setInterval(2000);
-            } else {
-                mLocationOption.setInterval(interval);
+        try {
+            if (mLocationClient == null) {
+                mLocationClient = new AMapLocationClient(context);
             }
-        }
-        //设置定位参数
-        mLocationClient.setLocationOption(mLocationOption);
-        //设置定位监听
-        mLocationClient.setLocationListener(amapLocation -> {
-            if (amapLocation != null) {
-                if (amapLocation.getErrorCode() == 0) {
-                    //定位成功回调信息，设置相关消息
-                    // amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
-                    // amapLocation.getLatitude();//获取纬度
-                    // amapLocation.getLongitude();//获取经度
-                    // amapLocation.getAccuracy();//获取精度信息
-                    // SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-                    // Date date = new Date(amapLocation.getTime());
-                    // df.format(date);//定位时间
-
-                    // amapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
-                    // amapLocation.getCountry();//国家信息
-                    // amapLocation.getProvince();//省信息
-                    // amapLocation.getCity();//城市信息
-                    // amapLocation.getDistrict();//城区信息
-                    // amapLocation.getStreet();//街道信息
-                    // amapLocation.getStreetNum();//街道门牌号信息
-                    // amapLocation.getCityCode();//城市编码
-                    // amapLocation.getAdCode();//地区编码
-                    // amapLocation.getAoiName();//获取当前定位点的AOI信息
-
-                    if (listener != null) {
-                        listener.onLocationChanged(amapLocation);
-                    }
-
+            //初始化定位参数
+            if (mLocationOption == null) {
+                mLocationOption = new AMapLocationClientOption();
+            }
+            //设置返回地址信息，默认为true
+            mLocationOption.setNeedAddress(true);
+            //设置定位模式为高精度模式，Battery_Saving为低功耗模式，Device_Sensors是仅设备模式
+            mLocationOption.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+            if (isLoop) {
+                //设置定位间隔,单位毫秒,默认为2000ms
+                if (interval == 0) {
+                    mLocationOption.setInterval(2000);
                 } else {
-                    //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
-                    LogUtil.e("获取定位信息错误：AmapError", "location Error, ErrCode:"
-                            + amapLocation.getErrorCode() + ", errInfo:"
-                            + amapLocation.getErrorInfo());
+                    mLocationOption.setInterval(interval);
                 }
             }
-        });
+            //设置定位参数
+            mLocationClient.setLocationOption(mLocationOption);
+            //设置定位监听
+            mLocationClient.setLocationListener(amapLocation -> {
+                if (amapLocation != null) {
+                    if (amapLocation.getErrorCode() == 0) {
+                        //定位成功回调信息，设置相关消息
+                        // amapLocation.getLocationType();//获取当前定位结果来源，如网络定位结果，详见定位类型表
+                        // amapLocation.getLatitude();//获取纬度
+                        // amapLocation.getLongitude();//获取经度
+                        // amapLocation.getAccuracy();//获取精度信息
+                        // SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+                        // Date date = new Date(amapLocation.getTime());
+                        // df.format(date);//定位时间
 
-        // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
-        // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
-        // 在定位结束后，在合适的生命周期调用onDestroy()方法
-        // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
-        //启动定位
-        mLocationClient.startLocation();
+                        // amapLocation.getAddress();//地址，如果option中设置isNeedAddress为false，则没有此结果，网络定位结果中会有地址信息，GPS定位不返回地址信息。
+                        // amapLocation.getCountry();//国家信息
+                        // amapLocation.getProvince();//省信息
+                        // amapLocation.getCity();//城市信息
+                        // amapLocation.getDistrict();//城区信息
+                        // amapLocation.getStreet();//街道信息
+                        // amapLocation.getStreetNum();//街道门牌号信息
+                        // amapLocation.getCityCode();//城市编码
+                        // amapLocation.getAdCode();//地区编码
+                        // amapLocation.getAoiName();//获取当前定位点的AOI信息
+
+                        if (listener != null) {
+                            listener.onLocationChanged(amapLocation);
+                        }
+
+                    } else {
+                        //显示错误信息ErrCode是错误码，errInfo是错误信息，详见错误码表。
+                        LogUtil.e("获取定位信息错误：AmapError", "location Error, ErrCode:"
+                                + amapLocation.getErrorCode() + ", errInfo:"
+                                + amapLocation.getErrorInfo());
+                    }
+                }
+            });
+
+            // 此方法为每隔固定时间会发起一次定位请求，为了减少电量消耗或网络流量消耗，
+            // 注意设置合适的定位时间的间隔（最小间隔支持为1000ms），并且在合适时间调用stopLocation()方法来取消定位请求
+            // 在定位结束后，在合适的生命周期调用onDestroy()方法
+            // 在单次定位情况下，定位无论成功与否，都无需调用stopLocation()方法移除请求，定位sdk内部会移除
+            //启动定位
+            mLocationClient.startLocation();
+        } catch (Exception ignored) {
+        }
     }
 
     /**
