2e427fe53cff807a6d9ffe17028610adce23a2dc
xujixing01@souche.com <19900713op>
2021-12-31 15:55:33 +0800

    UI接口

---
+++ src/main/java/com/android/helper/interfaces/ActivityUiInterface.java
@@ -0,0 +1,14 @@
+package com.android.helper.interfaces;
+
+/**
+ * @author : 流星
+ * @CreateDate: 2021/12/31-3:46 下午
+ * @Description:
+ */
+public interface ActivityUiInterface {
+
+    /**
+     * Activity初始化view
+     */
+    void initView();
+}
---
+++ src/main/java/com/android/helper/interfaces/FragmentUiInterface.java
@@ -0,0 +1,18 @@
+package com.android.helper.interfaces;
+
+import android.view.View;
+
+/**
+ * @author : 流星
+ * @CreateDate: 2021/12/31-3:47 下午
+ * @Description:
+ */
+public interface FragmentUiInterface {
+
+    /**
+     * Fragment初始化view
+     *
+     * @param rootView fragment的根布局
+     */
+    void initView(View rootView);
+}
