ff5013ec606c0ec5eef42c989a9425b1281e0a4f
xujixing01@souche.com <19900713op>
2021-12-31 13:00:44 +0800

    UIListener的优化

--- src/main/java/com/android/helper/interfaces/UIListener.java
+++ src/main/java/com/android/helper/interfaces/UIListener.java
@@ -6,6 +6,11 @@ import android.view.View;
 public interface UIListener {
 
     /**
+     * 初始化状态栏
+     */
+    void initStatusBar();
+
+    /**
      * 在setContentView之前的调用方法，用于特殊的使用
      */
     void onBeforeCreateView();
@@ -29,5 +34,10 @@ public interface UIListener {
      * 初始化数据
      */
     void initData(Bundle savedInstanceState);
+
+    /**
+     * 初始化initData之后的操作，在某些场景中去使用
+     */
+    void initDataAfter();
 
 }
