85e16c1136ac254e4c3904119a9a3b2ca76346bc
xjxliuxing@163.com <xjxliuxing@163.com>
2022-03-28 22:44:46 +0800

    “”

--- src/main/java/com/android/helper/utils/FileUtil.java
+++ src/main/java/com/android/helper/utils/FileUtil.java
@@ -9,6 +9,7 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Environment;
+import android.provider.Settings;
 import android.text.TextUtils;
 
 import androidx.activity.result.ActivityResult;
