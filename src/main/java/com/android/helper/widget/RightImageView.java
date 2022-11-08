909c4e5d00d63bf8dc89b3231affb0f02ea3a6ab
xjxliuxing <xjxliuxing@163.com>
2022-11-09 00:26:10 +0800

    左右 view的重构

--- src/main/java/com/android/helper/widget/RightImageView.java
+++ src/main/java/com/android/helper/widget/RightImageView.java
@@ -16,8 +16,6 @@ import com.android.helper.utils.BitmapUtil;
  */
 public class RightImageView extends androidx.appcompat.widget.AppCompatImageView {
 
-    private int measuredWidth, measuredHeight;
-
     public RightImageView(Context context) {
         super(context);
     }
@@ -26,39 +24,27 @@ public class RightImageView extends androidx.appcompat.widget.AppCompatImageView
         super(context, attrs);
     }
 
-    @Override
-    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-
-        // 高度就是view最大的高度
-        measuredHeight = resolveSize(heightMeasureSpec, MeasureSpec.getSize(heightMeasureSpec));
-
-        Bitmap bitmapForImageView = BitmapUtil.getBitmapForImageView(this);
-        if (bitmapForImageView != null) {
-            int width = bitmapForImageView.getWidth();
-            int height = bitmapForImageView.getHeight();
-
-            // 求出bitmap的宽高
-            float scaleBitmap = (float) width / height;
-
-            // 宽 / 高 = 比例  ，宽度 = 高度 * 比例
-            measuredWidth = (int) (measuredHeight * scaleBitmap);
-
-            // LogUtil.e("measuredHeight:" + measuredHeight + "   width:" + width + "   height:" + height);
-            // LogUtil.e("measuredWidth:" + measuredWidth + "   measuredHeight:" + measuredHeight + "   scaleBitmap:" + scaleBitmap);
-            setMeasuredDimension(measuredWidth / 2, measuredHeight);
-        }
-    }
 
     @SuppressLint("DrawAllocation")
     @Override
     protected void onDraw(Canvas canvas) {
 
-        Rect src = new Rect(measuredWidth / 2, 0, measuredWidth, measuredHeight);
-        Rect des = new Rect(0, 0, measuredWidth / 2, measuredHeight);
-        Bitmap bitmap = BitmapUtil.getBitmapForScale(BitmapUtil.getBitmapForImageView(this), measuredWidth, measuredHeight);
+        int measuredWidth = getMeasuredWidth();
+        int measuredHeight = getMeasuredHeight();
+
+        Bitmap forImageView = BitmapUtil.getBitmapForImageView(this);
+
+        Bitmap bitmap = BitmapUtil.getBitmapForScale(forImageView, measuredWidth, measuredHeight);
 
         if (bitmap != null) {
+            int width = bitmap.getWidth();
+            int height = bitmap.getHeight();
+
+            int dx = width / 2;
+
+            Rect src = new Rect(0, 0, width, height);
+            Rect des = new Rect(-dx, 0, width - dx, height);
+
             canvas.drawBitmap(bitmap, src, des, null);
         }
     }
