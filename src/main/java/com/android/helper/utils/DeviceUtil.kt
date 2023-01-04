4ecdd9806f830be0d102968a2b5f7a429ab4263f
XJX <xjxliuxing@163.com>
2023-01-04 18:13:54 +0800

    DeviceUtil.kt

---
+++ src/main/java/com/android/helper/utils/DeviceUtil.kt
@@ -0,0 +1,45 @@
+package com.android.helper.utils
+
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Build
+import android.provider.Settings
+import java.util.*
+
+/**
+ * @author : 流星
+ * @CreateDate: 2023/1/4-17:58
+ * @Description:
+ */
+object DeviceUtil {
+
+    @SuppressLint("HardwareIds")
+    fun getAndroidId(context: Context): String {
+        return Settings.Secure.getString(context.contentResolver, Settings.Secure.ANDROID_ID)
+    }
+
+    fun getFingerprint(): String {
+        // 设备的唯一标识。由设备的多个信息拼接合成
+        val fingerprint = Build.FINGERPRINT
+        LogUtil.e("设备的唯一识别码为：$fingerprint")
+        return fingerprint
+    }
+
+    fun getDeviceSN(): String? {
+        return Build.SERIAL
+    }
+
+    fun getDeviceId(context: Context): String? {
+        val androidId = getAndroidId(context)
+
+        LogUtil.e("设备的android——id为：$androidId")
+
+        // 设备的唯一标识。由设备的多个信息拼接合成
+        val fingerprint = Build.FINGERPRINT
+        LogUtil.e("设备的唯一识别码为：$fingerprint")
+        val value = androidId + fingerprint
+        val md5: String = MD5Utils.getMD5String(value)
+        LogUtil.e("设备的唯一识别码的Md5为：$md5")
+        return md5
+    }
+}
